cf7be151ed65479fd3a148f0f16fc4824a57241d
Jake Roughton <112968037+JakeRoughton@users.noreply.github.com>
2023-07-04 23:59:17 -0400

    Add files via upload

--- Components/NavBar.js
+++ Components/NavBar.js
@@ -1,165 +1,43 @@
-import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import Menu from '@mui/material/Menu';
-import MenuIcon from '@mui/icons-material/Menu';
-import Container from '@mui/material/Container';
-import Button from '@mui/material/Button';
-import MenuItem from '@mui/material/MenuItem';
-import AdbIcon from '@mui/icons-material/Adb';
-import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
-import {Link} from "react-router-dom"
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 
-const pages = ['about', 'gallery', 'RequestForm', 'Financing', 'blogs' ];
-const settings = ['Register', 'Login', 'Logout'];
-
-function ResponsiveAppBar() {
-  const [anchorElNav, setAnchorElNav] = React.useState(null);
-  const [anchorElUser, setAnchorElUser] = React.useState(null);
-
-  const handleOpenNavMenu = (event) => {
-    setAnchorElNav(event.currentTarget);
-  };
-
-
-  const handleCloseNavMenu = () => {
-    setAnchorElNav(null);
-  };
-
-  const handleCloseUserMenu = () => {
-    setAnchorElUser(null);
-  };
-
+function CollapsibleExample() {
   return (
-    <AppBar className ='navBar' position="static">
-      <Container maxWidth="xl">
-        <Toolbar disableGutters>
-        <img src="https://i.imgur.com/vdowI17.png" href="/" alt="Logo" style={{ md: 'flex', marginRight: '8px', maxWidth:'40px' }}/>
-          <Typography
-            variant="h6"
-            noWrap
-            component="a"
-            sx={{
-              mr: 2,
-              display: { xs: 'none', md: 'flex' },
-              fontFamily: 'monospace',
-              fontWeight: 700,
-              letterSpacing: '.3rem',
-              color: 'white',
-              textDecoration: 'none',
-              href:'/'
-            }}
-          >
-          </Typography>
-
-          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
-            <IconButton
-              size="large"
-              aria-label="account of current user"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleOpenNavMenu}
-              color="inherit"
-            >
-              <MenuIcon />
-            </IconButton>
-            <Menu
-              id="menu-appbar"
-              anchorEl={anchorElNav}
-              anchorOrigin={{
-                vertical: 'bottom',
-                horizontal: 'left',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'left',
-              }}
-              open={Boolean(anchorElNav)}
-              onClose={handleCloseNavMenu}
-              sx={{
-                display: { xs: 'block', md: 'none' },
-              }}
-            >
-              {pages.map((page) => (
-                <MenuItem key={page} onClick={handleCloseNavMenu}>
-                  <Typography textAlign="center">
-                    <Link
-                    style={{textDecoration: "none", color:"white"}}
-                     to={`/${page}`}>{page}
-                    </Link>
-                  </Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box>
-          <AdbIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
-          <Typography
-            variant="h5"
-            noWrap
-            component="a"
-            href=""
-            sx={{
-              mr: 2,
-              display: { xs: 'flex', md: 'none' },
-              flexGrow: 1,
-              fontFamily: 'monospace',
-              fontWeight: 700,
-              letterSpacing: '.3rem',
-              color: 'inherit',
-              textDecoration: 'none',
-            }}
-          >
-            LOGO
-          </Typography>
-          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-            {pages.map((page) => (
-              <Button
-                key={page}
-                onClick={handleCloseNavMenu}
-                sx={{ my: 2, color: 'white', display: 'block' }}
-              >
-                <Link style={{textDecoration: "none", color:"white"}} to={`/${page}`}>{page}
-                </Link>
-              </Button>
-            ))}
-          </Box>
-
-          <Box sx={{ flexGrow: 0 }}>
-            <Menu
-              sx={{ mt: '45px' }}
-              id="menu-appbar"
-              anchorEl={anchorElUser}
-              anchorOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              open={Boolean(anchorElUser)}
-              onClose={handleCloseUserMenu}
-            >
-              {settings.map((setting) => (
-                <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                  <Typography textAlign="center">{setting}</Typography>
-                </MenuItem>
-              ))}
-          <IconButton color="inherit" aria-label="add to shopping cart">
-        <AddShoppingCartIcon />
-      </IconButton>
-            </Menu>
-          </Box>
-          <Button color="inherit" href="/login">Login</Button>
-          <Button color="inherit" href="/register">Register</Button>
-        </Toolbar>
-      </Container>
-    </AppBar>
+    <Navbar id="navbar" collapseOnSelect expand="lg" variant="dark">
+      <Navbar.Brand href="/home">
+            <img
+              src="https://i.imgur.com/vdowI17.png"
+              width="30"
+              height="30"
+              className="d-inline-block align-top"
+              alt="React Bootstrap logo"
+            />
+          </Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="/about">About</Nav.Link>
+            <Nav.Link href="/gallery">Gallery</Nav.Link>
+            <Nav.Link href="/financing">Financing</Nav.Link>
+            <Nav.Link href="/blogs">Blogs</Nav.Link>
+            <NavDropdown bg="success" variant="light" title="Request Forms" id="collasible-nav-dropdown">
+              <NavDropdown.Item href="/requestform">Standard Request Form</NavDropdown.Item>
+              <NavDropdown.Item href="/requestformcustom">
+                Custom Request Form
+              </NavDropdown.Item>
+              <NavDropdown.Item href="/requestformvirtual">Inspection Request Form</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+          <Nav>
+            <Nav.Link href="/register">Register</Nav.Link>
+            <Nav.Link eventKey={2} href="/login">
+              Login
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+    </Navbar>
   );
 }
-export default ResponsiveAppBar;
+
+export default CollapsibleExample;
